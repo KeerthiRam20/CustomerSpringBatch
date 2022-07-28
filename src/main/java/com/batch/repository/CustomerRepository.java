2b649ceeda40df8ebb774afc63c5baa945428ccf
KeerthiRam20 <keerthisaiguru2000@gmail.com>
2022-07-28 15:35:01 +0530

    Initial commit

---
+++ src/main/java/com/batch/repository/CustomerRepository.java
@@ -0,0 +1,7 @@
+package com.batch.repository;
+
+import com.batch.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository  extends JpaRepository<Customer,Integer> {
+}
